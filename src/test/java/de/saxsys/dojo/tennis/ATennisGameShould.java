de24949d67ac20614d6fa6316c3621d86f40ec7c
Kai.Zickmann <Kai.Zickmann@saxsys.de>
2011-04-04 16:40:24 +0200

    Added test 40-30 to fix the functionality a little bit more.

--- src/test/java/de/saxsys/dojo/tennis/ATennisGameShould.java
+++ src/test/java/de/saxsys/dojo/tennis/ATennisGameShould.java
@@ -9,62 +9,73 @@ import org.junit.Test;
 
 public class ATennisGameShould {
 
-	private TennisGame tennisGame;
+    private TennisGame tennisGame;
 
-	@Before
-	public void init() {
-		tennisGame = new TennisGame();
+    @Before
+    public void init() {
+        tennisGame = new TennisGame();
 
-	}
+    }
 
-	@Test
-	public void returnLoveAll() {
-		assertTrue(tennisGame.getScore().equals("Love All"));
+    @Test
+    public void returnLoveAll() {
+        assertTrue(tennisGame.getScore().equals("Love All"));
 
-	}
+    }
 
-	@Test
-	public void return15Love() throws Exception {
-		tennisGame.addPointForPlayerOne();
-		assertThat(tennisGame.getScore(), is("Fifteen Love"));
-	}
+    @Test
+    public void return15Love() throws Exception {
+        tennisGame.addPointForPlayerOne();
+        assertThat(tennisGame.getScore(), is("Fifteen Love"));
+    }
 
-	@Test
-	public void return30Love() throws Exception {
-		tennisGame.addPointForPlayerOne();
-		tennisGame.addPointForPlayerOne();
+    @Test
+    public void return30Love() throws Exception {
+        tennisGame.addPointForPlayerOne();
+        tennisGame.addPointForPlayerOne();
 
-		assertThat(tennisGame.getScore(), is("Thirty Love"));
-	}
-	@Test
-	public void return40Love() throws Exception {
-		tennisGame.addPointForPlayerOne();
-		tennisGame.addPointForPlayerOne();
-		tennisGame.addPointForPlayerOne();
-		
-		assertThat(tennisGame.getScore(), is("Fourty Love"));
-	}
+        assertThat(tennisGame.getScore(), is("Thirty Love"));
+    }
 
-	@Test
-	public void returnLove15() throws Exception {
-		tennisGame.addPointForPlayerTwo();
-		assertThat(tennisGame.getScore(), is("Love Fifteen"));
-	}
+    @Test
+    public void return40Love() throws Exception {
+        tennisGame.addPointForPlayerOne();
+        tennisGame.addPointForPlayerOne();
+        tennisGame.addPointForPlayerOne();
 
-	@Test
-	public void returnLove30() throws Exception {
-		tennisGame.addPointForPlayerTwo();
-		tennisGame.addPointForPlayerTwo();
-		assertThat(tennisGame.getScore(), is("Love Thirty"));
-	}
-	
-	@Test
-	public void returnThirtyAll() throws Exception {
-		tennisGame.addPointForPlayerOne();
-		tennisGame.addPointForPlayerOne();
-		tennisGame.addPointForPlayerTwo();
-		tennisGame.addPointForPlayerTwo();
-		
-		assertThat(tennisGame.getScore(), is("Thirty All"));
-	}
+        assertThat(tennisGame.getScore(), is("Fourty Love"));
+    }
+
+    @Test
+    public void returnLove15() throws Exception {
+        tennisGame.addPointForPlayerTwo();
+        assertThat(tennisGame.getScore(), is("Love Fifteen"));
+    }
+
+    @Test
+    public void returnLove30() throws Exception {
+        tennisGame.addPointForPlayerTwo();
+        tennisGame.addPointForPlayerTwo();
+        assertThat(tennisGame.getScore(), is("Love Thirty"));
+    }
+
+    @Test
+    public void returnThirtyAll() throws Exception {
+        tennisGame.addPointForPlayerOne();
+        tennisGame.addPointForPlayerOne();
+        tennisGame.addPointForPlayerTwo();
+        tennisGame.addPointForPlayerTwo();
+        assertThat(tennisGame.getScore(), is("Thirty All"));
+    }
+
+    @Test
+    public void returnFourtyThirty() throws Exception {
+        tennisGame.addPointForPlayerOne();
+        tennisGame.addPointForPlayerTwo();
+        tennisGame.addPointForPlayerOne();
+        tennisGame.addPointForPlayerTwo();
+        tennisGame.addPointForPlayerOne();
+        assertThat(tennisGame.getScore(), is("Fourty Thirty"));
+    }
+
 }
